f020e52e455fdb7f35cda27125e32bfc7892c54c
ahsanfarooq210 <ahsanfarooq210@gmail.com>
2020-05-22 14:19:38 +0500

    initial commit

--- app/src/main/java/com/example/database_project_admin/show_order_activity.java
+++ app/src/main/java/com/example/database_project_admin/show_order_activity.java
@@ -4,13 +4,50 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.luseen.spacenavigation.SpaceItem;
+import com.luseen.spacenavigation.SpaceNavigationView;
+import com.luseen.spacenavigation.SpaceOnClickListener;
+
 public class show_order_activity extends AppCompatActivity
 {
+    private SpaceNavigationView navigationView;;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_show_order_activity);
+
+        navigationView=findViewById(R.id.space);
+        navigationView.initWithSaveInstanceState(savedInstanceState);
+        //navigationView.addSpaceItem(new SpaceItem("",R.drawable.icons8_delete_file_64px_2));
+        navigationView.addSpaceItem(new SpaceItem("", R.drawable.icons8_sorting_64px));
+        navigationView.setCentreButtonIcon(R.drawable.icons8_product_documents_64px_1);
+        navigationView.addSpaceItem(new SpaceItem("", R.drawable.icons8_new_company_80px));
+        // navigationView.addSpaceItem(new SpaceItem("",R.drawable.icons8_delete_document_64px));
+        navigationView.setCentreButtonSelectable(true);
+        navigationView.setCentreButtonSelected();
+
+        navigationView.setSpaceOnClickListener(new SpaceOnClickListener()
+        {
+            @Override
+            public void onCentreButtonClick()
+            {
+
+            }
+
+            @Override
+            public void onItemClick(int itemIndex, String itemName)
+            {
+
+            }
+
+            @Override
+            public void onItemReselected(int itemIndex, String itemName)
+            {
+
+            }
+        });
     }
 }
